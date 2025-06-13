891b9d45cb60c463615fd5c4922fb30df716bf93
Dev-Luigy <luigygabriel18a@gmail.com>
2025-06-13 15:04:21 -0300

    refactor(utils): Changes TreeFunctions to TreeUtils.

---
+++ structures/Trees/utils/treeUtils.cpp
@@ -0,0 +1,28 @@
+#ifndef TREE_UTILS_HPP
+#define TREE_UTILS_HPP
+
+#include "../../../interfaces/core/Node.hpp"
+#include <iostream>
+#include <stack>
+#include <vector>
+
+template <typename T> static void printValues(Node<T> *node) {
+  if (node) {
+    std::cout << node->key << " ";
+  }
+}
+template <typename T>
+static void insertNodesInStack(Node<T> *node, std::stack<Node<T> *> &stack) {
+  if (node) {
+    stack.push(node);
+  }
+}
+
+template <typename T>
+static void insertNodesInVector(Node<T> *node, std::vector<Node<T> *> &vector) {
+  if (node) {
+    vector.push_back(node);
+  }
+}
+
+#endif
