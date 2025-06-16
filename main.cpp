3ab7427eb1ea9fa1ef2e8307df3d2ad476bdaaa0
Dev-Luigy <luigygabriel18a@gmail.com>
2025-06-16 14:40:48 -0300

    docs(Main): changes main to test values.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,16 @@ int main() {
   RedBlack<int> tree;
   RedBlack<int> tree1;
 
-  std::vector<int> numbers = {15, 10, 20, 8, 12, 25, 6, 11, 13, 22, 27};
+  std::vector<int> numbers = {11, 2, 14, 1, 7, 5, 8, 15, 4};
   for (int i = 0; i < (int)numbers.size(); i++) {
     tree.insert(numbers[i]);
     tree.show();
+  }
+
+  for (int i = 0; i < (int)numbers.size(); i++) {
+    std::cout << "Sucessor de: " << numbers[i]
+              << " é: " << tree.successor(numbers[i]) << std::endl;
+    std::cout << "predecessor de: " << numbers[i]
+              << " é: " << tree.predecessor(numbers[i]) << std::endl;
   }
 }
