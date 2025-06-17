8fb2d1573c66c755cc62293900f06d5eff7d9354
Dev-Luigy <luigygabriel18a@gmail.com>
2025-06-17 13:36:21 -0300

    feat(FixupContext): Now all actions and responsabilities are in Context classes.

--- interfaces/trees/rotatable/FixupContext.hpp
+++ interfaces/trees/rotatable/FixupContext.hpp
@@ -1,10 +1,16 @@
 #ifndef FIXUP_CONTEXT_HPP
 #define FIXUP_CONTEXT_HPP
 
-template <typename CaseType> class FixupContext {
+#include "../../core/Node.hpp"
+
+template <typename CaseType, typename T> class FixupContext {
 public:
   virtual ~FixupContext() = default;
   virtual CaseType getCase() const = 0;
+  virtual Node<T> *
+  useCaseAction(Node<T> *m_root) = 0; // Always should return a leaf.
+  virtual Node<T> *
+  fixupAction(Node<T> *m_root) = 0; // Always should return a leaf.
 };
 
 #endif
