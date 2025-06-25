147447cf990030a40d24a9fd6b972ba3c819a44c
Dev-Luigy <luigygabriel18a@gmail.com>
2025-06-25 17:27:09 -0300

    feat(DataStructure): Created int execute.

--- interfaces/core/DataStructure.hpp
+++ interfaces/core/DataStructure.hpp
@@ -1,16 +1,13 @@
+
 #ifndef DATASTRUCTURE_HPP
 #define DATASTRUCTURE_HPP
 
 #include "KeyExtractor.hpp"
 #include "Node.hpp"
+#include <string>
 #include <vector>
+
 template <typename T> class DataStructure {
-
-  // https://learn.microsoft.com/pt-br/cpp/cpp/decltype-cpp?view=msvc-170
-  using KeyType = decltype(KeyExtractor<T>::getKey(
-      std::declval<T>())); // this will return type of getKey, if key is int,
-                           // then keytype = int
-
 public:
   virtual void insert(T value) = 0;
   virtual void remove(T value) = 0;
@@ -19,22 +16,37 @@ public:
   virtual void show() = 0;
   virtual void clear() = 0;
   virtual Node<T> *getNode(const T &value) = 0;
-  virtual ~DataStructure() {};
+  virtual ~DataStructure() {}
 
-  template <typename DataStructure>
-  static void execute(DataStructure &structure,
+  template <typename Structure>
+  static void execute(Structure &structure,
                       const std::vector<std::string> &words) {
     using WordCount = std::pair<std::string, int>;
 
     for (const auto &word : words) {
-      WordCount key{word, 0};
+      WordCount probe{word, 0};
 
-      if (structure.contains(key)) {
-        auto *node = structure.getNode(key);
+      if (structure.contains(probe)) {
+        auto *node = structure.getNode(probe);
         if (node)
           node->key.second += 1;
       } else {
         structure.insert({word, 1});
+      }
+
+      structure.show();
+    }
+  }
+
+  template <typename Structure>
+  static void execute(Structure &structure, const std::vector<int> &numbers) {
+    for (const auto &number : numbers) {
+      if (structure.contains(number)) {
+        auto *node = structure.getNode(number);
+        if (node)
+          node->key++;
+      } else {
+        structure.insert(number);
       }
 
       structure.show();
--- main.cpp
+++ main.cpp
@@ -1,19 +1,55 @@
+#include "structures/Trees/AVLTree.hpp"
 #include "structures/Trees/RedBlack.hpp"
-
-using namespace std;
-
+#include <string>
+#include <utility>
 int main() {
-  RedBlack<int> tree;
+  AVLTree<std::pair<std::string, int>> avlWords;
+  RedBlack<std::pair<std::string, int>> rbWords;
+  AVLTree<int> avlInt;
+  RedBlack<int> rbInt;
 
-  std::vector<int> numbers = {11, 2, 14, 1, 7, 5, 8, 15, 4};
-  for (int i = 0; i < (int)numbers.size(); i++) {
-    tree.insert(numbers[i]);
-    tree.show();
-  }
+  std::vector<int> valores = {30, 20, 40, 10, 25, 35, 50, 5, 15};
+  std::vector<std::string> words = {
+      "livro",    "texto",    "palavra",  "árvore",   "código",   "programa",
+      "dados",    "função",   "variável", "classe",   "livro",    "texto",
+      "programa", "dados",    "livro",    "função",   "código",   "variável",
+      "classe",   "palavra",  "texto",    "árvore",   "dados",    "programa",
+      "função",   "livro",    "variável", "código",   "classe",   "palavra",
+      "programa", "dados",    "texto",    "livro",    "função",   "variável",
+      "classe",   "palavra",  "código",   "árvore",   "dados",    "livro",
+      "texto",    "programa", "função",   "variável", "classe",   "palavra",
+      "código",   "livro",    "árvore",   "texto",    "dados",    "função",
+      "programa", "classe",   "variável", "palavra",  "código",   "livro",
+      "texto",    "função",   "programa", "dados",    "variável", "classe",
+      "palavra",  "árvore",   "código",   "livro",    "texto",    "programa",
+      "função",   "dados",    "classe",   "variável", "palavra",  "código",
+      "livro",    "árvore",   "texto",    "dados",    "função",   "programa",
+      "classe",   "variável", "palavra",  "código",   "livro",    "texto",
+      "função",   "programa", "dados",    "variável", "classe",   "palavra",
+      "árvore",   "código",   "livro",    "texto",    "programa", "função",
+      "dados",    "classe",   "variável", "palavra",  "código",   "livro",
+      "árvore",   "texto",    "dados",    "função",   "programa", "classe",
+      "variável", "palavra",  "código",   "livro",    "texto",    "função",
+      "programa", "dados",    "variável", "classe",   "palavra",  "árvore",
+      "código",   "livro",    "texto",    "programa", "função",   "dados",
+      "classe",   "variável", "palavra",  "código",   "livro",    "árvore",
+      "texto",    "dados",    "função",   "programa", "classe",   "variável",
+      "palavra",  "código",   "livro",    "texto",    "função",   "programa",
+      "dados",    "variável", "classe",   "palavra",  "árvore",   "código",
+      "livro",    "texto",    "programa", "função",   "dados",    "classe",
+      "variável", "palavra",  "código",   "livro",    "árvore",   "texto",
+      "dados",    "função",   "programa", "classe",   "variável", "palavra",
+      "código",   "livro",    "texto",    "função",   "programa", "dados",
+      "variável", "classe",   "palavra",  "árvore",   "código",   "livro",
+      "texto",    "programa", "função",   "dados",    "classe",   "variável",
+      "palavra",  "código",   "livro",    "árvore",   "texto",    "dados",
+      "função",   "programa"};
 
-  for (int i = 0; i < (int)numbers.size(); i++) {
-    std::cout << "Removendo: " << numbers[i] << std::endl;
-    tree.remove(numbers[i]);
-    tree.show();
-  }
+  DataStructure<std::pair<std::string, int>>::execute(avlWords, words);
+  DataStructure<std::pair<std::string, int>>::execute(rbWords, words);
+
+  DataStructure<int>::execute(avlInt, valores);
+  DataStructure<int>::execute(rbInt, valores);
+
+  return 0;
 }
