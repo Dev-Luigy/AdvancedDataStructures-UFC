cbd1db3f58b31fae575f38b21ce3c11c61a0bc09
Dev-Luigy <luigygabriel18a@gmail.com>
2025-06-25 16:43:37 -0300

    feat(DataStructure): Created execute method to update second value at pair types.

--- interfaces/core/DataStructure.hpp
+++ interfaces/core/DataStructure.hpp
@@ -2,6 +2,8 @@
 #define DATASTRUCTURE_HPP
 
 #include "KeyExtractor.hpp"
+#include "Node.hpp"
+#include <vector>
 template <typename T> class DataStructure {
   using KeyType = decltype(KeyExtractor<T>::getKey(
       std::declval<T>())); // this will return type of getKey, if key is int,
@@ -14,7 +16,28 @@ public:
   virtual bool isEmpty() = 0;
   virtual void show() = 0;
   virtual void clear() = 0;
+  virtual Node<T> *getNode(const T &value) = 0;
   virtual ~DataStructure() {};
+
+  template <typename DataStructure>
+  static void execute(DataStructure &structure,
+                      const std::vector<std::string> &words) {
+    using WordCount = std::pair<std::string, int>;
+
+    for (const auto &word : words) {
+      WordCount key{word, 0};
+
+      if (structure.contains(key)) {
+        auto *node = structure.getNode(key);
+        if (node)
+          node->key.second += 1;
+      } else {
+        structure.insert({word, 1});
+      }
+
+      structure.show();
+    }
+  }
 };
 
 #endif
